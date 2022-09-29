772e5e47c97d40a773d05fdae74b1bbdcca54ad6
NazarethSebothoma <33572798@student365.msfed.nwu.ac.za>
2022-09-29 15:44:45 +0200

    repositories

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -0,0 +1,20 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZoneRepository : GenericRepository<Zone>, IZoneRepository
+    {
+        public ZoneRepository(ConnectedOfficeContext context) : base(context)
+        {
+
+        }
+        public Zone GetMostRecentZone()
+        {
+            return _context.Zone.OrderByDescending(Zone => Zone.DateCreated).FirstOrDefault();
+        }
+
+    }
+}
